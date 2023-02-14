ab2fc381e4658c4d8aaaea63971b2193154f0207
mailynmailyn <mlpuittinen@gmail.com>
2023-02-14 07:56:42 -0500

    main

---
+++ Main.java
@@ -0,0 +1,14 @@
+class Main{
+
+    public static void main(String[] args) throws Exception{
+
+        //Preprocessing
+        Preprocessing p = new Preprocessing();
+
+        p.main();
+
+        
+
+    }
+
+}
